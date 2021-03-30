238b15fc2c8f7f9786e4ef32c74c8cf0ef6fb7b3
roberto <rjsdnrsla@naver.com>
2021-03-30 11:32:41 +0900

    start!

---
+++ src/components/Movie.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function Movie() {
+    return (
+        <div className="movie">
+            This is movie component
+            
+        </div>
+    )
+}
+
+export default Movie
