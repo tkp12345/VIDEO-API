934112b69e13134902f78b5646dfa6219f6fd070
roberto <rjsdnrsla@naver.com>
2021-04-01 14:20:40 +0900

    api 사진이미지 받아오기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React ,{useEffect, useState} from 'react'
 
 import Movie from './components/Movie';
 
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7ecd0b11bc4cd387a22b43cb37086584";
+  
+  const IMG_API = "https://image.tmdb.org/t/p/w1280";
+  const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=7ecd0b11bc4cd387a22b43cb37086584&query=";
+
+
 function App() {
 
-  const movies = ['1' , '2' , '3'];
+  const [movies, setMovies] = useState([]);
+
+  useEffect(()=>{
+    fetch(FEATURED_API)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      setMovies(data.results);
+    });
+  
+  }, []);
 
   return (
     <div>
       {
-        movies.map(movie => (
-          <Movie/>
+        movies.length > 0 && movies.map(movie => (
+          <Movie key={movie.id} {...movie} />
         ))
       }
       
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,15 @@
 import React from 'react'
 
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
-function Movie() {
-    return (
-        <div className="movie">
-            This is movie component
-            
-        </div>
-    )
-}
+const Movie = ({title, poster_path, overview, 
+    vote_average}) =>(
+         <div className="movie">
+             <img src={IMG_API + poster_path} alt={title}/>
+    </div>
+    );
+
+ 
 
 export default Movie
+ 
