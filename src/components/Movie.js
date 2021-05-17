09af416c279a3f877a0356a0199b1371b97ba0a9
tkp12345 <tkp1@hanmail.net>
2021-05-18 07:06:32 +0900

    210518

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,7 +5,19 @@ const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const Movie = ({title, poster_path, overview, 
     vote_average}) =>(
          <div className="movie">
-             <img src={IMG_API + poster_path} alt={title}/>
+             <div className="movie-content">
+               <img src={IMG_API + poster_path} alt={title}/>
+             
+             <div className="movie-info">
+                 <h3>{title}</h3>
+                 <span>{vote_average}</span>
+             </div>
+
+             <div className="movie-over image-blur">
+               
+                 <p>{overview}</p>
+             </div>
+             </div>
     </div>
     );
 
